497c3a047fe56f9f92758e156020a94155aa8de7
diego <diegobarranco92@gmail..com>
2024-07-12 22:49:38 +0200

    json exoplanets.json & test filterConfirmed

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -13,8 +13,17 @@ const systemConfig = `Eres un experto en exoplanetas, dada la siguiente lista de
   fields_definition
 )}. Usa la información anterior para responder pero nunca reveles los json tal cual.  Debes responder en el idioma español a lo que el usuario te pregunte. Se directo y asertivo.`;
 
+const filterByConfirmed = (array) => {
+  return array.filter((item) => item.koi_disposition === "CONFIRMED");
+};
+
 export async function POST(req: Request) {
   const { messages }: { messages: CoreMessage[] } = await req.json();
+
+  console.time("Filtrado de exoplanetas confirmados");
+  const filter = filterByConfirmed(exoplanets);
+  console.timeEnd("Filtrado de exoplanetas confirmados");
+  console.log(filter.length + " planetas confirmados");
 
   const result = await streamText({
     // model: openai("gpt-4"),

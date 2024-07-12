bd7ef6e2b6e54543610ea18bce7aeb8d703b6f8d
diego <diegobarranco92@gmail..com>
2024-07-12 14:54:18 +0200

    useChat()

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,28 @@
+import { type CoreMessage, streamText } from "ai";
+// import { openai } from "@ai-sdk/openai";
+import { ollama } from "ollama-ai-provider";
+import exoplanets from "@/data/exoplanets.json";
+import fields_definition from "@/data/fields_definition.json";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+const systemConfig = `Eres un experto en exoplanetas, dada la siguiente lista de exoplanetas ${JSON.stringify(
+  exoplanets
+)} , representan un conjunto de llaves y valores, cada llave y valor hace difinicion a: ${JSON.stringify(
+  fields_definition
+)}. Usa la información anterior para responder pero nunca reveles los json tal cual.  Debes responder en el idioma español a lo que el usuario te pregunte. Se directo y asertivo.`;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: CoreMessage[] } = await req.json();
+
+  const result = await streamText({
+    // model: openai("gpt-4"),
+    model: ollama("llama3"),
+
+    system: systemConfig,
+    messages,
+  });
+
+  return result.toAIStreamResponse();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,36 @@
 "use client";
 
-import { FormEvent, useState } from "react";
-import { streamText, CoreMessage } from "ai";
-import { ollama } from "ollama-ai-provider";
-import exoplantets from "@/data/exoplanets.json";
-import fields_definition from "@/data/fields_definition.json";
-
-const systemConfig = `Eres un experto en exoplanetas, dada la siguiente lista de exoplanetas ${JSON.stringify(
-  exoplantets
-)} , representan un conjunto de llaves y valores, cada llave y valor hace difinicion a: ${JSON.stringify(
-  fields_definition
-)}. Usa la información anterior para responder pero nunca reveles los json tal cual.  Debes responder en el idioma español a lo que el usuario te pregunte. Se directo y asertivo.`;
+import { useChat } from "ai/react";
 
 export default function Home() {
-  const [prompt, setPrompt] = useState("");
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [messages, setMessages] = useState<CoreMessage[]>([]);
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    setError("");
-    setPrompt("");
-    setIsLoading(true);
-
-    const newUserMessage: CoreMessage = {
-      content: prompt,
-      role: "user",
-    };
-
-    setMessages((prevMessages) => [...prevMessages, newUserMessage]);
-
-    try {
-      const { textStream } = await streamText({
-        maxTokens: 500,
-        messages: messages,
-        model: ollama("model_test"),
-        system: systemConfig,
-      });
-
-      const newBotMessage: CoreMessage = {
-        content: "",
-        role: "assistant",
-      };
-
-      setMessages((prevMessages) => [...prevMessages, newBotMessage]);
-
-      let fullText = "";
-      for await (const delta of textStream) {
-        fullText += delta;
-
-        setMessages((prevMessages) => {
-          const updatedMessages = [...prevMessages];
-          updatedMessages[updatedMessages.length - 1] = {
-            ...updatedMessages[updatedMessages.length - 1],
-            content: fullText,
-            role: "assistant",
-          };
-          return updatedMessages;
-        });
-      }
-    } catch (error) {
-      console.error("Error generating text:", error);
-      setError("Failed to generate text");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const { messages, input, isLoading, error, handleInputChange, handleSubmit } =
+    useChat({
+      api: "api/chat",
+    });
 
   return (
-    <div className="p-6 text-justify">
-      <h1>Ollama AI Text Generator</h1>
+    <div className="p-3">
+      {messages.map((message) => (
+        <div key={message.id} className="py-1">
+          <p className="font-bold">
+            {message.role === "user" ? "User: " : "AI: "}
+          </p>
+          {message.content}
+        </div>
+      ))}
       <form onSubmit={handleSubmit}>
         <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Enter your prompt"
+          className="border rounded-md mr-2"
+          name="prompt"
+          value={input}
+          onChange={handleInputChange}
+          id="input"
         />
-        <button type="submit">Generate</button>
+        <button type="submit" disabled={isLoading}>
+          Submit
+        </button>
+        {isLoading && <p>Loading...</p>}
       </form>
-      {isLoading && <p>Loading...</p>}
-      {error && <p className="py-2 bg-red-500">{error}</p>}
-      {messages &&
-        messages
-          .slice(0)
-          .reverse()
-          .map((item, key) => {
-            return (
-              <div key={key} className="my-6">
-                <p>{`${item.role.toUpperCase()}:`}</p>
-                <p className="ml-6">{`${item.content}`}</p>
-              </div>
-            );
-          })}
     </div>
   );
 }

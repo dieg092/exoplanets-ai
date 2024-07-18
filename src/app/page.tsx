1f9ff3c6f96e0e5ae95800095dc5970cc9bb40e0
Dylan Gonzalez <dyg09032003@gmail.com>
2024-07-17 21:44:53 -0500

    Reduced boilerplate and fix overlap camera

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,13 @@
 "use client";
-import { Exoplanet } from "@/components/3d/Exoplanet";
-import { Universe } from "@/components/3d/Universe";
 import { ChatWrapper } from "@/components/chat/ChatWrapper";
-import { CameraControls } from "@react-three/drei";
+import { Scene } from "@/components/3d/Scene";
 import { Canvas } from "@react-three/fiber";
 
 const Page = () => {
   return (
     <div className="w-full h-screen">
       <Canvas className="bg-black w-full h-full">
-        <CameraControls minDistance={5} maxDistance={10} />
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <directionalLight color="red" position={[0, 0, 5]} intensity={0.1} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        {/* Earth prueba */}
-        <Exoplanet position={[0, 0, 0]} scale={1} textureName="earth" />
-        {/* Sun prueba */}
-        <Exoplanet position={[0, 0, -90]} scale={25} textureName="sun" />
-        {/* Universe sphere example */}
-        <Universe position={[0, 0, 0]} />
+        <Scene />
       </Canvas>
       <ChatWrapper />
     </div>
---
+++ src/components/3d/Scene.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { CameraControls } from "@react-three/drei";
+import { Exoplanet } from "@/components/3d/Exoplanet";
+import { useEffect, useRef } from "react";
+import { useControls, button, folder } from "leva";
+import { calculateCameraDistance } from "@/utils/calculateCameraDistance";
+
+export const Scene = () => {
+  const EXOPLANETSCALE = 2;
+  const cameraControlsRef = useRef<any>(null);
+
+  useEffect(() => {
+    const distanceZ = calculateCameraDistance(EXOPLANETSCALE);
+    // put camera on target mesh and position
+    cameraControlsRef.current?.setLookAt(
+      ...[0, 0, distanceZ],
+      ...[0, 0, 0],
+      true
+    );
+  }, []);
+
+  // Floating window - comment this disabled
+  useControls({
+    setLookAt: folder(
+      {
+        vec4: { value: [1, 2, 3], label: "position" },
+        vec5: { value: [1, 1, 0], label: "target" },
+        "setLookAt(…position, …target)": button((get) =>
+          cameraControlsRef.current?.setLookAt(
+            ...get("setLookAt.vec4"),
+            ...get("setLookAt.vec5"),
+            true
+          )
+        ),
+      },
+      { collapsed: true }
+    ),
+    reset: button(() => cameraControlsRef.current?.reset(true)),
+  });
+
+  return (
+    <>
+      <CameraControls
+        minDistance={calculateCameraDistance(EXOPLANETSCALE)}
+        maxDistance={310}
+        ref={cameraControlsRef}
+      />
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <directionalLight color="red" position={[0, 0, 5]} intensity={0.1} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+
+      {/* Exoplanet */}
+      <Exoplanet
+        rotationX={0}
+        rotationY={0.0005}
+        position={[0, 0, 0]}
+        scale={EXOPLANETSCALE}
+        texture="/exoplanets/earth.jpg"
+        side={0}
+      />
+
+      {/* Universe */}
+      <Exoplanet
+        rotationX={0.00005}
+        rotationY={0.00005}
+        position={[0, 0, 0]}
+        scale={600}
+        texture="/universe.jpg"
+        side={1}
+      />
+    </>
+  );
+};

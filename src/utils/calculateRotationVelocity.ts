9ffa23ba7faeb85944c78e9979fe24740db7a275
diego <diegobarranco92@gmail..com>
2024-07-20 23:14:39 +0200

    real rotation

--- src/utils/calculateRotationVelocity.ts
+++ src/utils/calculateRotationVelocity.ts
@@ -1,5 +1,6 @@
+const dayInSeconds = 24 * 60 * 60;
 export const calculateRotationVelocity = (orbitPeriod: number) => {
-  const angularMediaVelocity = (2 * Math.PI) / orbitPeriod;
+  const angularMediaVelocity = (2 * Math.PI) / (orbitPeriod * dayInSeconds);
 
   return angularMediaVelocity;
 };

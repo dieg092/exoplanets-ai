ed773479400b48ce077131600c68947534b4ebf7
diego <diegobarranco92@gmail..com>
2024-07-21 01:00:01 +0200

    comentario corregido en calculo de rotación Y de exoplaneta

--- src/utils/calculateRotationVelocity.ts
+++ src/utils/calculateRotationVelocity.ts
@@ -1,5 +1,5 @@
 export const calculateRotationVelocity = (rotationPeriodDays: number) => {
-  const degreesPerDay = 360 / rotationPeriodDays; // 360 / 172800 ≈ 0.0020833 grados por segundo
+  const degreesPerDay = 360 / rotationPeriodDays; // 360 / 172800 ≈ 0.0020833 grados por dia
   const degreesPerSecond = degreesPerDay / 86400; // 86400 segundos en un día
   const radiansPerSecond = degreesPerSecond * (Math.PI / 180); // convertir a radianes
 

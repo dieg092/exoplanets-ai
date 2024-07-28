bbe9aece8d8be08ab11bca7e3ab7076b4b9a7ada
Dylan Gonzalez <dyg09032003@gmail.com>
2024-07-28 16:12:19 -0500

    Add tools to modify the speed and display a list of exoplanets

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,11 +3,13 @@ import { createOpenAI } from "@ai-sdk/openai"
 import exoplanetsData from "@/data/exoplanets.json"
 import { z } from "zod"
 import {
+  fastVelocity,
   filterByConfirmed,
   filterByMajorOrbit,
   filterByMinorOrbit,
   filterByUnConfirmed,
   findExoplanet,
+  getListExoplanetsName,
   getRandomExoplanet,
 } from "@/utils/dataActions"
 import { Exoplanet } from "@/definition"
@@ -15,13 +17,12 @@ import { Exoplanet } from "@/definition"
 export const maxDuration = 30
 const exoplanets: Exoplanet[] = exoplanetsData as Exoplanet[]
 
-const systemConfig = `Eres un profesor de astronomía. 
-Guias al usuario en el aprendizaje sobre exoplanetas y de la tierra de forma divertida, gamificada y siempre das el siguiente paso si no tiene la iniciativa el usuario. 
+const systemConfig = `Eres un profesor de astronomía. Tu rol es guiar al usuario en el aprendizaje de los exoplanetas y también de la información de la tierra de forma divertida mostrando emojis en cada explicación y siempre debes dar el siguiente paso si el usuario no tiene iniciativa en preguntar. Ten en cuenta que todos los exoplanetas giran a velocidad de la vida real, asi que debes preguntarle al usuario no solo si quiere ver otros exoplanetas, si no, también si quiere aumentar la velocidad de giro o mantenerla en estado normal o si quiere que le muestres una lista de los exoplanetas disponibles. 
 Te ayudas para explicar y enseñar de un escenario en 3D del universo que se va moviendo a cada exoplaneta según tus respuestas. 
 Tienes conocimiento de toda la base de datos de exoplanetas de la NASA. Incluyendo la de esta URL: https://exoplanetarchive.ipac.caltech.edu/docs/API_kepcandidate_columns.html
 No hables de ningún tema que no esté relacionado con exoplanetas o con la tierra.
 Debes responder en el idioma en el que el usuario te pregunte. 
-Se directo y asertivo.`
+Se directo, asertivo y entretenido.`
 
 interface Message {
   role: "user" | "assistant" | "system"
@@ -51,7 +52,7 @@ export async function POST(req: Request) {
       tools: {
         exoplanets_confirmed: tool({
           description:
-            "Muestra el total de exoplanetas confirmados por la NASA",
+            "Muestra cuantos exoplanetas confirmados hay por la NASA",
           parameters: z.object({}),
           execute: async () => {
             return {
@@ -98,8 +99,63 @@ export async function POST(req: Request) {
               // Exoplanet not found
               return {
                 updateScene: false,
-                data: "null",
+                data: "No se ha encontrado el exoplaneta solicitado",
               }
+            }
+          },
+        }),
+        exoplanet_standar_velocity: tool({
+          description:
+            "Ajusta la velocidad orbital del exoplaneta que se ha mostrado o la velocidad de giro del exoplaneta mostrado a una velocidad baja o velocidad normal",
+          parameters: z.object({ exoplanet_name: z.string() }),
+          execute: async ({ exoplanet_name }) => {
+            const exoplanet = findExoplanet(exoplanets, exoplanet_name)
+
+            if (exoplanet !== null) {
+              return {
+                updateScene: true,
+                data: exoplanet,
+              }
+            } else {
+              // Exoplanet not found
+              return {
+                updateScene: false,
+                data: "No se ha encontrado el exoplaneta solicitado",
+              }
+            }
+          },
+        }),
+        exoplanet_fast_velocity: tool({
+          description:
+            "Ajusta la velocidad orbital del exoplaneta que se ha mostrado o la velocidad de giro del exoplaneta mostrado a una velocidad mas rápida",
+          parameters: z.object({ exoplanet_name: z.string() }),
+          execute: async ({ exoplanet_name }) => {
+            const exoplanet = fastVelocity(exoplanets, exoplanet_name)
+
+            if (exoplanet !== null) {
+              return {
+                updateScene: true,
+                data: exoplanet,
+              }
+            } else {
+              // Exoplanet not found
+              return {
+                updateScene: false,
+                data: "No se ha encontrado el exoplaneta solicitado",
+              }
+            }
+          },
+        }),
+        exoplanet_show_list_names: tool({
+          description:
+            "Muestra el nombre de todos los exoplanetas disponibles o muestra una lista de los exoplanetas o si te pregunta cuales son los exoplanetas",
+          parameters: z.object({}),
+          execute: async ({}) => {
+            return {
+              updateScene: false,
+              data:
+                "Al final colocale un emoji a cada exoplaneta y di que es una lista corta, que si quiere puedes generar nuevamente la lista" +
+                getListExoplanetsName(exoplanets, 10),
             }
           },
         }),

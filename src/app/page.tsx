0986d7994f63b019b9454871cd716955707cc874
diego <diegobarranco92@gmail..com>
2024-07-28 13:19:58 +0200

    Redirect a root si introducen parámetros en URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,19 @@
 import { ChatWrapper } from "@/components/chat/ChatWrapper"
 import { Scene } from "@/components/3d/Scene"
 import { Canvas } from "@react-three/fiber"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 const Page = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const { searchParams } = new URL(window.location.href)
+    if (searchParams && searchParams.toString()) {
+      router.replace("/")
+    }
+  }, [router])
+
   return (
     <div className="w-full h-screen overflow-hidden relative">
       <h1 className="font-bold text-2xl sm:text-4xl absolute z-50 sm:left-6 left-1/2 transform -translate-x-1/2 sm:transform-none sm:translate-x-0 top-3 text-white">

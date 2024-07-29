3304a2798e52fd33c6b2ab5b2840adbd106c135d
Dylan Gonzalez <dyg09032003@gmail.com>
2024-07-28 23:30:43 -0500

    Chat controll music and add more tracks

--- src/utils/saltMusic.ts
+++ src/utils/saltMusic.ts
@@ -1,7 +1,7 @@
 import { SOUND_EXOPLANET_PATH } from "@/config"
 
 export const saltMusic = () => {
-  const randomMusic = Math.round(Math.random() * (2 - 1) + 1)
+  const randomMusic = Math.round(Math.random() * (7 - 1) + 1)
   const musicPath = `${SOUND_EXOPLANET_PATH}/${randomMusic}.mp3`
 
   return musicPath
